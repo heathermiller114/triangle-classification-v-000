0ba9a03f6209542d9005e78756812c48942c5f1e
Heather Miller <heather.miller114@gmail.com>
2018-10-02 15:11:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,25 @@
 class Triangle
   
-  def initialize(length_side1, length_side2, length_side3)
+  attr_accessor :length_side1, :length_side2, :length_side3
   
+  def initialize(length_side1:, length_side2:, length_side3:)
+    @length_side1 = length_side1
+    @length_side2 = length_side2
+    @length_side3 = length_side3
+  end
+  
+  def kind
+    if @length_side1 == @length_side2 && @length_side1 == @length_side3
+      :equilateral
+    elsif @length_side1 == @length_side2 && @length_side1 != @length_side3 || @length_side2 == @length_side3 && @length_side2 != @length_side3
+      :isosceles
+    elsif @length_side1 != @length_side2 && @length_side1 != @length_side3
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  
+  end
 end
+
