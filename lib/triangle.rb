d1d04353e70a97c0951df593ea98326945256343
Heather Miller <heather.miller114@gmail.com>
2018-10-02 15:17:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
   
   def kind
-    if 
+    if @length_side1 = 0 || @length_side2 = 0 || @length_side3 = 0
     elsif @length_side1 == @length_side2 && @length_side1 == @length_side3
       :equilateral
     elsif @length_side1 == @length_side2 && @length_side1 != @length_side3 || @length_side2 == @length_side3 && @length_side2 != @length_side1 || @length_side1 == @length_side3 && @length_side1 != @length_side2
